b7a70c6e051ef6ed7a17225a355f68e71f82a1e1
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 00:32:03 +0530

    Modified the Makefile

--- components/common.sh
+++ components/common.sh
@@ -2,7 +2,6 @@ checkRootUser() {
   USER_ID=$(id -u)
   if [ "$USER_ID" -ne 0 ]; then
     echo "You are supposed to be running this script as sudo"
-  else
-    echo OK
+    exit
   fi
 }
--- components/frontend.sh
+++ components/frontend.sh
@@ -4,10 +4,6 @@ source components/common.sh
 
 checkRootUser
 
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ]; then
-  echo "You are supposed to be running this script as sudo"
-else
  yum install nginx -y
  systemctl enable nginx
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
@@ -19,5 +15,5 @@ else
  rm -rf frontend-main README.md
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
  systemctl restart nginx
-fi
+
 

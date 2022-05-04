48b088b2dfa7de9248729d6a4270552763eeb2e2
Win10 <yeddulaparineetha@gmail.com>
2022-05-04 23:49:09 +0530

    Modified the Makefile

--- components/frontend.sh
+++ components/frontend.sh
@@ -2,7 +2,6 @@
 
  yum install nginx -y
  systemctl enable nginx
- systemctl start nginx
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
  cd /usr/share/nginx/html
  rm -rf *

79f30e1e7845ac890c26e95ed58c137c80502665
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 18:02:20 +0530

    Modified the common.sh file

--- components/frontend.sh
+++ components/frontend.sh
@@ -4,36 +4,36 @@ source components/common.sh
 checkRootUser
 
 ECHO "Installing Nginx"
-yumm install nginx -y >> ${LOG_FILE}
+yumm install nginx -y &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Downloading the Nginx"
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >> ${LOG_FILE}
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
  statusCheck $?
 
  cd /usr/share/nginx/html
 
  ECHO "Removing the old files"
- rm -rf * >> ${LOG_FILE}
+ rm -rf * &>>${LOG_FILE}
  statusCheck $?
 
 
  ECHO "Extracting the Zip Content"
- unzip /tmp/frontend.zip >> ${LOG_FILE}
+ unzip /tmp/frontend.zip &>>${LOG_FILE}
  statusCheck $?
 
  ECHO "Copying the extracted content"
  mv frontend-main/* .
  mv static/* .
- rm -rf frontend-main README.md >> ${LOG_FILE}
+ rm -rf frontend-main README.md &>>${LOG_FILE}
  statusCheck $?
 
  ECHO "Copying the Roboshop nginx config"
- mv localhost.conf /etc/nginx/default.d/roboshop.conf >> ${LOG_FILE}
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
  statusCheck $?
 
  ECHO "Start Nginx Service"
- systemctl enable nginx >> ${LOG_FILE}
+ systemctl enable nginx &>>${LOG_FILE}
  systemctl restart nginx
  statusCheck $?
 

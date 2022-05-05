81213b7b1025bdd06d167cdfa601e7d0a1778e35
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 11:08:38 +0530

    Modified the common.sh file

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,41 +1,40 @@
 #!/usr/bin/env bash
-
 source components/common.sh
 
 checkRootUser
 
- echo Installing Nginx
- yum install nginx -y >/tmp/roboshop.log
- statusCheck() $?
+echo "Installing Nginx"
+yum install nginx -y >/tmp/roboshop.log
+statusCheck $?
 
- echo "Downloading the Nginx"
+echo "Downloading the Nginx"
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
- statusCheck() $?
+ statusCheck $?
 
  cd /usr/share/nginx/html
 
  echo "Removing the old files"
  rm -rf * >/tmp/roboshop.log
- statusCheck() $?
+ statusCheck $?
 
 
  echo "Extracting the Zip Content"
  unzip /tmp/frontend.zip >/tmp/roboshop.log
- statusCheck() $?
+ statusCheck $?
 
  echo "Copying the extracted content"
  mv frontend-main/* .
  mv static/* .
  rm -rf frontend-main README.md >/tmp/roboshop.log
- statusCheck() $?
+ statusCheck $?
 
  echo "Copying the Roboshop nginx config"
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
- statusCheck() $?
+ statusCheck $?
 
  echo "Start Nginx Service"
  systemctl enable nginx >/tmp/roboshop.log
  systemctl restart nginx
- statusCheck() $?
+ statusCheck $?
 
 

fd0a7930eba0773a16dead8978c329ef7eab340b
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 22:14:17 +0530

    Modified the common.sh file

--- create-ec2.sh
+++ create-ec2.sh
@@ -16,7 +16,7 @@ fi
 
 AMI_ID = $(aws ec2 describe-images  --filters "Name=Centos-7-DevOps-Practice,Values=windows" --output table | grep ImageId | awk '{print $4}')
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options
+aws ec2 run-instances --image-id $(AMI_ID) --instance-type t3.micro --instance-market-options
 "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications
 "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE-NAME}]"
 "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE-NAME}]"

8fc168d9d65acfa37dd1824e89b5ad34f6aac2e3
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 10:58:51 +0530

    Modified the common.sh file

--- components/frontend.sh
+++ components/frontend.sh
@@ -6,71 +6,36 @@ checkRootUser
 
  echo Installing Nginx
  yum install nginx -y >/tmp/roboshop.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
- else
-   echo -e "\e[31mFAILURE\e[m0"
-   exit 1
-fi
+ statusCheck() $?
 
-echo "Downloading the Nginx"
+ echo "Downloading the Nginx"
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
- else
-   echo -e "\e[31mFAILURE\e[m0"
-   exit 1
-fi
+ statusCheck() $?
 
  cd /usr/share/nginx/html
 
  echo "Removing the old files"
  rm -rf * >/tmp/roboshop.log
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILURE\e[m0"
-    exit 1
- fi
+ statusCheck() $?
 
 
  echo "Extracting the Zip Content"
  unzip /tmp/frontend.zip >/tmp/roboshop.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
- else
-   echo -e "\e[31mFAILURE\e[m0"
-   exit 1
-fi
+ statusCheck() $?
 
  echo "Copying the extracted content"
  mv frontend-main/* .
  mv static/* .
  rm -rf frontend-main README.md >/tmp/roboshop.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
- else
-   echo -e "\e[31mFAILURE\e[m0"
-   exit 1
-fi
+ statusCheck() $?
 
  echo "Copying the Roboshop nginx config"
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
- else
-   echo -e "\e[31mFAILURE\e[m0"
-   exit 1
-fi
+ statusCheck() $?
 
  echo "Start Nginx Service"
  systemctl enable nginx >/tmp/roboshop.log
  systemctl restart nginx
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
- else
-   echo -e "\e[31mFAILURE\e[m0"
-   exit 1
-fi
+ statusCheck() $?
 
 

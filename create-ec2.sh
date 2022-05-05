e71ab7802ef6c07ef4a574eae4b46634f4c145f9
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 22:25:43 +0530

    Modified the common.sh file

--- create-ec2.sh
+++ create-ec2.sh
@@ -13,10 +13,9 @@ if [ $? -eq 0 ]; then
   echo "Instance already exists"
   exit 0
 fi
-
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print $4}')
 
-aws ec2 run-instances --image-id $(AMI_ID) --instance-type t3.micro --instance-market-options
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options
 "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications
 "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE-NAME}}]"
 "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE-NAME}}]" &>/dev/null

be2768ec0927ca7aa06f8076053c66e623239c23
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 23:15:43 +0530

    Modified the common.sh file

--- create-ec2.sh
+++ create-ec2.sh
@@ -18,3 +18,13 @@ AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Pr
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,
 InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=instance,
 Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" &>/dev/null
+
+sleep 10
+
+INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${INSTANCE_NAME} Name=state,Values=active --output table | grep InstanceId | awk
+'{print $4}')
+
+IPADDRESS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
+
+sed -e "s/COMPONENT/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z10124301UJ6NN38EWFHH --change-batch file:///tmp/record.json &>/dev/null

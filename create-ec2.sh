f1d458f1a2e71bfbbef6acc5db558f74baf648a5
Win10 <yeddulaparineetha@gmail.com>
2022-05-05 22:15:53 +0530

    Modified the common.sh file

--- create-ec2.sh
+++ create-ec2.sh
@@ -5,18 +5,18 @@ if [ -z $1 ]; then
   exit 1
 fi
 
-INSTANCE-NAME = $1
+INSTANCE_NAME=$1
 
-aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${INSTANCE-NAME} Name=State,Values=active --output table | grep InstanceId &>/dev/null
+aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${INSTANCE_NAME} Name=State,Values=active --output table | grep InstanceId &>/dev/null
 
 if [ $? -eq 0 ]; then
   echo "Instance already exists"
   exit 0
 fi
 
-AMI_ID = $(aws ec2 describe-images  --filters "Name=Centos-7-DevOps-Practice,Values=windows" --output table | grep ImageId | awk '{print $4}')
+AMI_ID=$(aws ec2 describe-images  --filters "Name=Centos-7-DevOps-Practice,Values=windows" --output table | grep ImageId | awk '{print $4}')
 
 aws ec2 run-instances --image-id $(AMI_ID) --instance-type t3.micro --instance-market-options
 "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications
-"ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE-NAME}]"
-"ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE-NAME}]"
+"ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE-NAME}}]"
+"ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE-NAME}}]"

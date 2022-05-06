8fb8505098e0d54c15a1904c17ef3b104dc11c93
Win10 <yeddulaparineetha@gmail.com>
2022-05-06 20:43:16 +0530

    Modified the common.sh file

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -3,5 +3,24 @@
 source components/common.sh
 checkRootUser
 
-COMPONENT=catalogue
-NODEJS
+
+
+
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
+
+ECHO "Configure the NodeJS Yum Repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Install the NodeJS"
+yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Add Application User"
+useradd roboshop
+statusCheck $?
+

0d056633ca5ac1de41398945e406d40ae354f5e7
Win10 <yeddulaparineetha@gmail.com>
2022-05-06 18:12:10 +0530

    Modified the common.sh file

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,28 +1,36 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 source components/common.sh
 
 checkRootUser
 
 ECHO "Setup MongoDB Yum repo"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Installing the MongoDB"
-yum install -y mongodb-org
+yum install -y mongodb-org &>>{LOG_FILE}
 statusCheck $?
 
 ECHO "Configure the listen in MongoDB configuration"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Start MongoDB Service"
+systemctl restart mongod &>>${LOG_FILE} && systemctl enable mongod &>>{LOG_FILE}
+statusCheck $?
+
+ECHO "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Extract Schema Zip"
+cd /tmp && unzip mongodb.zip &>>${LOG_FILE}
+statusCheck $?
+
+cd mongodb-main
+ECHO "Load Schema"
+mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
 statusCheck $?
 
 
-#systemctl enable mongod
-#systemctl start mongod
-#systemctl restart mongod
 
-#curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-#cd /tmp
-#unzip mongodb.zip
-#cd mongodb-main
-#mongo < catalogue.js
-#mongo < users.js

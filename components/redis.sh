96bf4269b81c740e024a2122b1b382f993f38fe7
Win10 <yeddulaparineetha@gmail.com>
2022-05-07 00:52:42 +0530

    Modified the common.sh file

---
+++ components/redis.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source components/common.sh
+checkRootUser
+
+ECHO "Configure Yum Repos"
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Install Redis"
+yum install redis-6.2.7 -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Updated Redis configuration"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+statusCheck $?
+
+ECHO "Start Redis service"
+systemctl enable redis &>>${LOG_FILE} && systemctl start redis &>>${LOG_FILE}
+statusCheck $?
+
+
+
+
+

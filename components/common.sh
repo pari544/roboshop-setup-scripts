831f2d4cd186a854802aeba60df0ed864fedd69d
Win10 <yeddulaparineetha@gmail.com>
2022-05-08 12:25:33 +0530

    Modified the common.sh file

--- components/common.sh
+++ components/common.sh
@@ -25,6 +25,35 @@ ECHO() {
   echo "$1"
 }
 
+APPLICATION_SETUP(){
+  id roboshop &>>${LOG_FILE}
+      if [ $? -ne 0 ]; then
+      ECHO "Add Application User"
+      useradd roboshop
+      statusCheck $?
+    fi
+
+    ECHO "Download Application Content"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
+    statusCheck $?
+
+    ECHO "Extract Application Archive"
+    cd /home/roboshop && rm -rf ${COMPONENT} &>>${LOG_FILE} && unzip /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT}
+    statusCheck $?
+
+}
+
+SYSTEMD_SETUP() {
+  ECHO "Update SystemD Configuration files"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/'/home/roboshop/${COMPONENT}/systemd.service
+  statusCheck $?
+
+  ECHO "Setup systemd service"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  systemctl daemon-reload &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE}
+  statusCheck $?
+}
+
 NODEJS() {
   ECHO "Configure the NodeJS Yum Repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
@@ -34,31 +63,25 @@ NODEJS() {
   yum install nodejs gcc-c++ -y &>>${LOG_FILE}
   statusCheck $?
 
-  id roboshop &>>${LOG_FILE}
-    if [ $? -ne 0 ]; then
-    ECHO "Add Application User"
-    useradd roboshop
-    statusCheck $?
-  fi
-
-  ECHO "Download Application Content"
-  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
-  statusCheck $?
-
-  ECHO "Extract Application Archive"
-  cd /home/roboshop && rm -rf ${COMPONENT} &>>${LOG_FILE} && unzip /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT}
-  statusCheck $?
+  APPLICATION_SETUP
 
   ECHO "Install NodeJS modules"
   cd /home/roboshop/${COMPONENT} && npm install &>>${LOG_FILE} && chown roboshop:roboshop  /home/roboshop/${COMPONENT} -R
   statusCheck $?
 
-  ECHO "Update SystemD Configuration files"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+  SYSTEMD_SETUP
+}
+
+JAVA() {
+  ECHO "Installing Java and Maven"
+  yum install maven -y &>>${LOG_FILE}
   statusCheck $?
 
-  ECHO "Setup systemd service"
-  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-  systemctl daemon-reload &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE}
+  APPLICATION_SETUP
+
+  ECHO "Compile Maven Package"
+  cd /home/roboshop/${COMPONENT} && mvn clean package && mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar &>>${LOG_FILE}
   statusCheck $?
+
+  SYSTEMD_SETUP
 }

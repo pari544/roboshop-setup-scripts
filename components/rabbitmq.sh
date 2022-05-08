d51d545f89f423903c354caf2900cbbe6122ab3d
Win10 <yeddulaparineetha@gmail.com>
2022-05-08 13:50:42 +0530

    Modified the common.sh file

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -3,21 +3,25 @@
 source components/common.sh
 checkRootUser
 
-ECHO "Setup Yum Repos"
+ECHO "Setup YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
 statusCheck $?
 
-ECHO "Install and RabbitMQ and Erlang"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>${LOG_FILE}
+ECHO "Install RabbitMQ & Erlang"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Start RabbitMQ Service"
-systemctl enable rabbitmq-server &>>${LOG_FILE} && systemctl start rabbitmq-server &>>${LOG_FILE}
+systemctl enable rabbitmq-server &>>${LOG_FILE}  && systemctl start rabbitmq-server &>>${LOG_FILE}
 statusCheck $?
 
-ECHO "Create an Application User"
-rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
-statusCheck $?
+rabbitmqctl list_users | grep roboshop &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
+  ECHO "Create an Application User"
+  rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
+  statusCheck $?
+fi
 
- # rabbitmqctl set_user_tags roboshop administrator
- # rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+ECHO "Setup Application User Persmissions"
+rabbitmqctl set_user_tags roboshop administrator  &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG_FILE}
+statusCheck $?

db324b05156d2a8c6c37a181bf42b30810a8d8ef
Win10 <yeddulaparineetha@gmail.com>
2022-05-08 12:39:31 +0530

    Modified the common.sh file

--- components/common.sh
+++ components/common.sh
@@ -1,19 +1,20 @@
 checkRootUser() {
   USER_ID=$(id -u)
 
-  if [ "$USER_ID" -ne 0 ]; then
-    echo -e "\e[31mYou are supposed to be running this script as sudo\e[0m"
+  if [ "$USER_ID" -ne "0" ]; then
+    echo -e "\e[31mYou are suppose to be running this script as sudo or root user\e[0m"
     exit 1
   fi
 }
 
+
 statusCheck() {
   if [ $1 -eq 0 ]; then
-     echo -e "\e[32mSUCCESS\e[0m"
-   else
-     echo -e "\e[31mFAILURE\e[m0"
-     echo "Check the error log in ${LOG_FILE}"
-     exit 1
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    echo "Check the error log in ${LOG_FILE}"
+    exit 1
   fi
 }
 
@@ -21,67 +22,69 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 ECHO() {
-  echo -e "===========================$1=========================\n" >>${LOG_FILE}
+  echo -e "=========================== $1 ===========================\n" >>${LOG_FILE}
   echo "$1"
 }
 
-APPLICATION_SETUP(){
+APPLICATION_SETUP() {
   id roboshop &>>${LOG_FILE}
-      if [ $? -ne 0 ]; then
-      ECHO "Add Application User"
-      useradd roboshop
-      statusCheck $?
-    fi
-
-    ECHO "Download Application Content"
-    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
+  if [ $? -ne 0 ]; then
+    ECHO "Add Application User"
+    useradd roboshop &>>${LOG_FILE}
     statusCheck $?
+  fi
 
-    ECHO "Extract Application Archive"
-    cd /home/roboshop && rm -rf ${COMPONENT} &>>${LOG_FILE} && unzip /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT}
-    statusCheck $?
+  ECHO "Download Application Content"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
+  statusCheck $?
 
+  ECHO "Extract Application Archive"
+  cd /home/roboshop && rm -rf ${COMPONENT} &>>${LOG_FILE} && unzip /tmp/${COMPONENT}.zip &>>${LOG_FILE}  && mv ${COMPONENT}-main ${COMPONENT}
+  statusCheck $?
 }
 
 SYSTEMD_SETUP() {
-  ECHO "Update SystemD Configuration files"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+  chown roboshop:roboshop /home/roboshop/${COMPONENT} -R
+
+  ECHO "Update SystemD Configuration Files"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
   statusCheck $?
 
-  ECHO "Setup systemd service"
-  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  ECHO "Setup SystemD Service"
+  mv /home/roboshop/${COMPONENT}/systemd.service  /etc/systemd/system/${COMPONENT}.service
   systemctl daemon-reload &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE}
   statusCheck $?
 }
 
+
 NODEJS() {
-  ECHO "Configure the NodeJS Yum Repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
+  ECHO "Configure NodeJS YUM Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${LOG_FILE}
   statusCheck $?
 
-  ECHO "Install the NodeJS"
-  yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+  ECHO "Install NodeJS"
+  yum install nodejs  gcc-c++ -y &>>${LOG_FILE}
   statusCheck $?
 
   APPLICATION_SETUP
 
-  ECHO "Install NodeJS modules"
-  cd /home/roboshop/${COMPONENT} && npm install &>>${LOG_FILE} && chown roboshop:roboshop  /home/roboshop/${COMPONENT} -R
+  ECHO "Install NodeJS Modules"
+  cd /home/roboshop/${COMPONENT} && npm install &>>${LOG_FILE}
   statusCheck $?
 
   SYSTEMD_SETUP
 }
 
 JAVA() {
-  ECHO "Installing Java and Maven"
+  ECHO "Installing Java & Maven"
   yum install maven -y &>>${LOG_FILE}
   statusCheck $?
 
   APPLICATION_SETUP
 
   ECHO "Compile Maven Package"
-  cd /home/roboshop/${COMPONENT} && mvn clean package && mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar &>>${LOG_FILE}
+  cd /home/roboshop/${COMPONENT} && mvn clean package &>>${LOG_FILE} && mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar &>>${LOG_FILE}
   statusCheck $?
 
   SYSTEMD_SETUP
-}
+}}

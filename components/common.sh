997e058859b5ffa75427a39b8ea4a789936397f1
Win10 <yeddulaparineetha@gmail.com>
2022-05-08 15:35:04 +0530

    Modified the common.sh file

--- components/common.sh
+++ components/common.sh
@@ -47,7 +47,8 @@ SYSTEMD_SETUP() {
   chown roboshop:roboshop /home/roboshop/${COMPONENT} -R
 
   ECHO "Update SystemD Configuration Files"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e
+  's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST /user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
   statusCheck $?
 
   ECHO "Setup SystemD Service"
@@ -90,16 +91,16 @@ JAVA() {
 }
 
 PYTHON() {
-  yum install python36 gcc python3-devel -y
-  useradd roboshop
-  cd /home/roboshop
-  $ curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
-  $ unzip /tmp/payment.zip
-  $ mv payment-main payment
-  cd /home/roboshop/payment
-  # pip3 install -r requirements.txt
-  mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-  # systemctl daemon-reload
-  # systemctl enable payment
-  # systemctl start payment
+  ECHO "Install Python"
+  yum install python36 gcc python3-devel -y &>>${LOG_FILE}
+  statusCheck $?
+
+  APPLICATION_SETUP
+
+  ECHO "Install the dependencies"
+  cd /home/roboshop/${COMPONENT} && pip3 install -r requirements.txt &>>${LOG_FILE} && mv  &>>${LOG_FILE}
+  statusCheck $?
+
+  SYSTEMD_SETUP
+
 }
